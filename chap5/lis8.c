6a0de54881248dbce9b9cbadf71bd2c2eab18d67
phnii <sudiryaya@gmail.com>
2022-04-26 14:44:39 +0900

    lis8.c

---
+++ chap5/lis8.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int flag[8];
+int pos[8];
+
+void print(void)
+{
+  for (int i = 0; i < 8; i++) {
+    printf("%2d", pos[i]);
+  }
+  putchar('\n');
+}
+
+void set(int i)
+{
+  for (int j = 0; j < 8; j++) {
+    if (!flag[j]) {
+      pos[i] = j;
+      if (i == 7)
+        print();
+      else {
+        flag[j] = 1;
+        set(i + 1);
+        flag[j] = 0;
+      }
+    }
+  }
+}
+
+int main(void)
+{
+  set(0);
+  return 0;
+}
