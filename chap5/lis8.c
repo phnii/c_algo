10e53dedfe3b689c25ae2a77d21c72db413bfc01
phnii <sudiryaya@gmail.com>
2022-04-26 14:49:08 +0900

    lis8.c solution

--- chap5/lis8.c
+++ chap5/lis8.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-int flag[8];
+int flag_a[8];    // 各行に王妃を配置済み
+int flag_b[15];   // /対角線に配置済み
+int flag_c[15];   // \対角線に配置済み
 int pos[8];
 
 void print(void)
@@ -14,14 +16,14 @@ void print(void)
 void set(int i)
 {
   for (int j = 0; j < 8; j++) {
-    if (!flag[j]) {
+    if (!flag_a[j] && !flag_b[i + j] && !flag_c[i - j + 7]) {
       pos[i] = j;
       if (i == 7)
         print();
       else {
-        flag[j] = 1;
+        flag_a[j] = flag_b[i + j] = flag_c[i - j + 7] = 1;
         set(i + 1);
-        flag[j] = 0;
+        flag_a[j] = flag_b[i + j] = flag_c[i - j + 7] = 0;
       }
     }
   }
